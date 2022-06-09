d1c89e9290ea9c8dc87148c30e29e59142f54dd8
Elliot Toman <web@asubtleweb.com>
2022-06-09 11:59:42 -0400

    Header updates

--- wp-content/themes/sentia_2022/searchform.php
+++ wp-content/themes/sentia_2022/searchform.php
@@ -1,7 +1,9 @@
-<div class="page-width">
+<div class="search-form">
     <form method="get" class="header-search" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-        <label>Search: </label>
-        <input type="text" class="field" name="s" id="s" />
-        <input type="submit" class="submit" name="submit" id="searchsubmit" value="&rarr;" />
+        <label>Search this Site: </label>
+        <div class="fields">
+            <input type="text" class="field" name="s" id="s" />
+            <input type="submit" class="submit" name="submit" id="searchsubmit" value="&rarr;" />
+        </div>
     </form>
 </div>

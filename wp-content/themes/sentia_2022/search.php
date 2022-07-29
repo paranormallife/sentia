692141e27a6adbcf7c98e3a570f2d90f5b31bf28
Elliot Toman <web@asubtleweb.com>
2022-07-29 15:19:30 -0400

    Search results

--- wp-content/themes/sentia_2022/search.php
+++ wp-content/themes/sentia_2022/search.php
@@ -5,7 +5,7 @@
 <!-- Search Template -->
 
 <main>
-    <div class="post-content search-results page-width">
+    <div class="post-content search-results page-width slim-width">
 
         <h1>Search Results for "<?php echo get_search_query(); ?>"</h1>
 
@@ -13,24 +13,17 @@
         echo '<ul class="articles">';
         while ( have_posts() ) {
             the_post(); 
-            $thumbnail = get_the_post_thumbnail_url( $post->ID, 'full' );
-            $thumbUrl = get_the_post_thumbnail_url( $post->ID, 'large' );
-            $placeholder = $theme . '/assets/images/placeholder.svg';
             $postType = get_post_type();
             $obj = get_post_type_object( $postType );
             $postTypeName = $obj->labels->name;
             if( $postType == 'post' ) {
-                $postTypeName = 'Ruminations';
-            }
-            // Use the featured image if there is one. Otherwise, use the placeholder:
-            if ( !empty( $thumbUrl ) ) {
-                $thumb = $thumbUrl;
-            } else {
-                $thumb = $placeholder;
+                $postTypeName = 'Blog';
+            } elseif ( $postType == 'page' ) {
+                $postTypeName = 'Page';
             }
             echo '<li class="'.$postType.'">';
-                echo '<a href="' . get_the_permalink() . '" style="background-image: url(\'' . $thumb . '\')" title="' . get_the_title() . '">';
-                    echo '<span class="article-type">'.$postTypeName.'</span>';
+                echo '<a href="' . get_the_permalink() . '" title="' . get_the_title() . '">';
+                    echo '<span class="article-type">'.$postTypeName.': </span>';
                     echo '<span>' . get_the_title() . '</span>';
                 echo '</a>';
             echo '</li>';

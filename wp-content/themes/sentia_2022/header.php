5e9cd1f6971926d202dfe8dde773f59e213dda79
Elliot Toman <web@asubtleweb.com>
2022-09-08 12:35:59 -0400

    Stylesheet updates

--- wp-content/themes/sentia_2022/header.php
+++ wp-content/themes/sentia_2022/header.php
@@ -21,9 +21,9 @@
 ?>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=<?= $hash; ?>" />
 <link rel="stylesheet" type="text/css" media="all" href="<?= $theme ?>/assets/fonts/stylesheet.css?v=<?= $hash; ?>" />
-<link rel="stylesheet" type="text/css" media="all" href="<?= $theme ?>/assets/fonts/base.css?v=<?= $hash; ?>" />
-<link rel="stylesheet" type="text/css" media="all" href="<?= $theme ?>/assets/fonts/tablet.css?v=<?= $hash; ?>" />
-<link rel="stylesheet" type="text/css" media="all" href="<?= $theme ?>/assets/fonts/desktop.css?v=<?= $hash; ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?= $theme ?>/assets/css/base.css?v=<?= $hash; ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?= $theme ?>/assets/css/tablet.css?v=<?= $hash; ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?= $theme ?>/assets/css/desktop.css?v=<?= $hash; ?>" />
 
 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="  crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
